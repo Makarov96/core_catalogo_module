48d5e0e25ee641baf3f7dc04226f8219effed082
Makarov96 <estev97@gmail.com>
2022-04-14 19:15:08 -0600

    implement a specific class to params

--- lib/feature/domain/usecases/set_favorite_product.dart
+++ lib/feature/domain/usecases/set_favorite_product.dart
@@ -3,16 +3,29 @@ import 'package:core_catalogo_module/core/usecase/use_case.dart';
 import 'package:core_catalogo_module/feature/data/model/request/set_favorite_request_model.dart';
 import 'package:core_catalogo_module/feature/domain/repositories/store_repository.dart';
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:injectable/injectable.dart';
 
 @lazySingleton
-class SetFavoriteProduct implements UseCase<bool, SetFavoriteRequestModel> {
-
+class SetFavoriteProduct implements UseCase<bool, FavoriteParams> {
   SetFavoriteProduct(this._storeRepository);
   final StoreRepository _storeRepository;
 
   @override
   Future<Either<GenericException, bool>> call(
-          SetFavoriteRequestModel params) async =>
-      _storeRepository.favoriteProduct(params);
+          FavoriteParams params) async =>
+      _storeRepository.favoriteProduct(
+        SetFavoriteRequestModel(
+          setBoolValue: SetBoolValue(isFavorite: params.isFavorite),
+          setPkColumn: SetPkColumn(id: params.productId),
+        ),
+      );
+}
+
+class FavoriteParams extends Equatable {
+  const FavoriteParams(this.isFavorite, this.productId);
+  final bool isFavorite;
+  final int productId;
+  @override
+  List<Object?> get props => [];
 }
